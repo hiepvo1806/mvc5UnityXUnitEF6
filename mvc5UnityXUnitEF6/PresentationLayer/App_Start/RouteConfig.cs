5c66fd9a62ddaa0c0751da3a996ce53a1c787dcf
hiep.vo@groovetechnology.com <hiepvo-groover>
2018-12-31 23:27:14 +0700

    updated

--- mvc5UnityXUnitEF6/PresentationLayer/App_Start/RouteConfig.cs
+++ mvc5UnityXUnitEF6/PresentationLayer/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PresentationLayer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Authors", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
