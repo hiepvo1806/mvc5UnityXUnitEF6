e6254ed7965610229ba783b05ae6fd5f561d8907
Hiep Jeff Vo <k3c6.hiep@gmail.com>
2019-01-02 18:25:52 +0700

    updated

--- mvc5UnityXUnitEF6/UnitTestProject1/SampleTestEFMock.cs
+++ mvc5UnityXUnitEF6/UnitTestProject1/SampleTestEFMock.cs
@@ -38,23 +38,9 @@ namespace UnitTestProject1
         [Fact]
         public void SampleEfTest()
         {
-            var author = new Author()
-            {
-                Age = 123,
-                Books = new List<Book>(),
-                FirstName = "Just Mocked First Name",
-                Id = 123,
-                LastName = "Just Mocked Last Name"
-            };
-            var dataSource = new List<Author>()
-            {
-                author
-            }; //<-- this will hold data
-           
-            var mockSet = new MockDbSet<Author>(dataSource);
-            var mockContext = new Mock<BookstoreContext>();
-            mockContext.Setup(c => c.Set<Author>()).Returns(mockSet.Object);
-            Setup(mockContext);
+            var setupResult =  Setup(null);
+            var mockSet = setupResult.MockSet;
+            var dataSource = setupResult.DataSource;
             //ACT
             _serviceUnderTest.Create(new AuthorVM()
             {
@@ -72,13 +58,42 @@ namespace UnitTestProject1
         }
 
 
-        private void Setup(Mock<BookstoreContext> mockContext)
+        private MockSetupResult<Author> Setup(List<Author> inputDataSource)
         {
+            var author = new Author()
+            {
+                Age = 123,
+                Books = new List<Book>(),
+                FirstName = "Just Mocked First Name",
+                Id = 123,
+                LastName = "Just Mocked Last Name"
+            };
+            var dataSource = new List<Author>()
+            {
+                author
+            };
+            if (inputDataSource != null)
+                dataSource = dataSource.Concat(inputDataSource).ToList(); //<-- this will hold data
+            var mockSet = new MockDbSet<Author>(dataSource);
+            var mockContext = new Mock<BookstoreContext>();
+            mockContext.Setup(c => c.Set<Author>()).Returns(mockSet.Object);
             AuthorRepo = new AuthorRepo(mockContext.Object);
             AuthorRepo = new AuthorRepo(mockContext.Object);
             Uow = new UnitOfWork(mockContext.Object);
             _uowService = new UnitOfWorkService(Uow);
             _serviceUnderTest = new AuthorService(_mapper, AuthorRepo);
+            return new MockSetupResult<Author>
+            {
+                DataSource = dataSource,
+                MockSet = mockSet
+            };
+
         }
+    }
+
+    public class MockSetupResult<T> where T:class
+    {
+        public MockDbSet<T> MockSet { get; set; }
+        public List<T> DataSource { get; set; }
     }
 }
