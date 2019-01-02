cbdbe6ae2ef188e6bf92e004e080d4cd1e7c38e6
Hiep Jeff Vo <k3c6.hiep@gmail.com>
2019-01-02 18:00:08 +0700

    add EF unit test

---
+++ mvc5UnityXUnitEF6/UnitTestProject1/Helper/MockDbSet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Moq;
+
+namespace UnitTestProject1.Helper
+{
+    public class MockDbSet<TEntity> : Mock<DbSet<TEntity>> where TEntity : class
+    {
+        public MockDbSet(List<TEntity> dataSource = null)
+        {
+            var data = (dataSource ?? new List<TEntity>());
+            var queryable = data.AsQueryable();
+
+            As<IQueryable<TEntity>>().Setup(e => e.Provider).Returns(queryable.Provider);
+            As<IQueryable<TEntity>>().Setup(e => e.Expression).Returns(queryable.Expression);
+            As<IQueryable<TEntity>>().Setup(e => e.ElementType).Returns(queryable.ElementType);
+            As<IQueryable<TEntity>>().Setup(e => e.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+            //Mocking the insertion of entities
+            Setup(_ => _.Add(It.IsAny<TEntity>())).Returns((TEntity arg) => {
+                data.Add(arg);
+                return arg;
+            });
+            //...the same can be done for other members like Remove
+
+
+        }
+    }
+}
---
+++ mvc5UnityXUnitEF6/UnitTestProject1/SampleTestEFMock.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataAccessLayer;
+using DataAccessLayer.Models;
+using DataAccessLayer.Repo;
+using Moq;
+using ServiceLayer;
+using ServiceLayer.Models;
+using ServiceLayer.Service;
+using UnitTestProject1.Helper;
+using UnitTestProject1.Model;
+using Xunit;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    public class SampleTestEFMock
+    {
+        protected IMapper _mapper { get; set; }
+        public SampleTestEFMock()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ServiceMapperProfile());
+            });
+            _mapper = config.CreateMapper();
+        }
+
+        [Fact]
+        public void SampleEfTest()
+        {
+            var author = new Author()
+            {
+                Age = 123,
+                Books = new List<Book>(),
+                FirstName = "Just Mocked First Name",
+                Id = 123,
+                LastName = "Just Mocked Last Name"
+            };
+
+            var dataSource = new List<Author>()
+            {
+                author
+            }; //<-- this will hold data
+           
+            var mockSet = new MockDbSet<Author>(dataSource);
+            var mockContext = new Mock<BookstoreContext>();
+
+            mockContext.Setup(c => c.Set<Author>()).Returns(mockSet.Object);
+            IAuthorRepo authorRepo = new AuthorRepo(mockContext.Object);
+            IUnitOfWork uow = new UnitOfWork(mockContext.Object);
+            IUnitOfWorkService uowService = new UnitOfWorkService(uow);
+            IAuthorService service = new AuthorService(_mapper,authorRepo);
+            //ACT
+            service.Create(new AuthorVM()
+            {
+                FirstName = "hiep",
+                Books = null,
+                Id = 12,
+                LastName = "Vo",
+                Age = 28
+            });
+            uowService.Commit();
+            
+            // ASSERT
+            mockSet.Verify(u => u.Add(It.IsAny<Author>()), Times.Once());
+            Assert.Contains(dataSource, x =>x.FirstName == "hiep"); //<-- shows mock actually added item
+        }
+    }
+}
