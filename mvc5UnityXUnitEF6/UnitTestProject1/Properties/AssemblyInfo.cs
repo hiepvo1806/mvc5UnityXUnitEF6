5ac73ba6a4d5120468123019abc7ac55489647aa
Hiep Jeff Vo <k3c6.hiep@gmail.com>
2019-01-02 11:02:24 +0700

    add UT project

---
+++ mvc5UnityXUnitEF6/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d0a973d-4b11-4634-ae47-04159efa041c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mvc5UnityXUnitEF6/UnitTestProject1/Sample/EqualExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTestProject.Sample
+{
+    public class EqualExample
+    {
+        [Fact]
+        public void EqualStringIgnoreCase()
+        {
+            string expected = "TestString";
+            string actual = "teststring";
+
+            Assert.False(actual == expected);
+            Assert.NotEqual(expected, actual);
+            Assert.Equal(expected, actual, StringComparer.CurrentCultureIgnoreCase);
+        }
+    }
+}
