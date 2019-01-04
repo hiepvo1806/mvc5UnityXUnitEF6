df4b7146379d984bc87368f505a32e192b0357d7
Hiep Jeff Vo <k3c6.hiep@gmail.com>
2019-01-04 11:21:23 +0700

    updated

---
+++ mvc5UnityXUnitEF6/UnitTestProject1/Sample/ParallelExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
+namespace UnitTestProject.ParallelSample
+{
+    [Collection("Our Test Collection #1")]
+    public class ParallelTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Thread.Sleep(3000);
+        }
+    }
+
+    [Collection("Our Test Collection #1")]
+    public class ParallelTest2
+    {
+        [Fact]
+        public void Test2()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
