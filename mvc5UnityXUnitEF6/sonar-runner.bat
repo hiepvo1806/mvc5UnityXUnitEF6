59364068fba097e159da57573823274a2495d1e8
hiepvo <k3c6.hiep@gmail.com>
2019-01-08 07:11:14 +0700

    updated

---
+++ mvc5UnityXUnitEF6/sonar-runner.bat
@@ -0,0 +1,5 @@
+D:\tool\sonar\SonarScanner.MSBuild.exe begin /k:"JeffTestProj"
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" /t:Rebuild
+
+D:\tool\sonar\SonarScanner.MSBuild.exe end
